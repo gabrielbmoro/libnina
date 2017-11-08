f854f2704ea4094dc35165eac6514fe98038eb03
Gabriel B Moro <tido4410@gmail.com>
2017-11-08 01:02:59 -0200

    scorep installation new version

--- scripts/scorep_installation.sh
+++ scripts/scorep_installation.sh
@@ -10,7 +10,38 @@ sudo apt install mpich -y
 
 sudo apt install openmpi-bin -y	
 
-sudo apt install papi-tools -y
+cd $HOME/Downloads/
+
+wget http://icl.utk.edu/projects/papi/downloads/papi-5.5.1.tar.gz
+
+tar vxf papi-5.5.1.tar.gz 
+
+mv papi-5.5.1 ../Programs/
+
+cd $HOME/Programs/papi-5.5.1/src/
+
+./configure
+
+make
+
+sudo make install
+
+cd $HOME/Downloads/
+
+wget http://www.vi-hps.org/upload/packages/opari2/opari2-2.0.2.tar.gz
+
+tar vxf opari2-2.0.2.tar.gz
+
+mv opari2-2.0.2 ../Programs/
+
+cd $HOME/Programs/opari2-2.0.2/
+
+./configure
+
+make
+
+sudo make install
+
 
 cd $HOME/Downloads/
 
@@ -18,9 +49,9 @@ wget http://pkgs.fedoraproject.org/repo/pkgs/libunwind/libunwind-1.1.tar.gz/fb4e
 
 tar vxf libunwind-1.1.tar.gz
 
-mv libunwind-1.1 ../Programas/
+mv libunwind-1.1 ../Programs/
 
-cd $HOME/Programas/libunwind-1.1/
+cd $HOME/Programs/libunwind-1.1/
 
 ./configure
 
@@ -34,26 +65,25 @@ wget http://www.vi-hps.org/upload/packages/scorep/scorep-3.0.tar.gz
 
 tar vxf scorep-3.0.tar.gz
 
-mv scorep-3.0 $HOME/Programas/
+mv scorep-3.0 $HOME/Programs/
 
-cd $HOME/Programas/scorep-3.0/
+cd $HOME/Programs/scorep-3.0/
 
-programsPath="$(pwd)"
 
-./configure --prefix=$(pwd)/build --without-gui --with-opari2=/opt/opari2/bin/ \
+./configure --prefix="$(pwd)/build" --without-gui --with-opari2=/opt/opari2/bin/ \
  --with-mpi=openmpi --without-shmen --enable-papi --with-papi-lib=/usr/local/lib/ \
  --with-papi-header=/usr/local/include/ --with-libunwind=$programsPath/libunwind-1.1/ \
- --with-libunwind-include=$programsPath/libunwind-1.1/include/ \
- --with-libunwind-lib=$programsPath/libunwind-1.1/ader=/usr/local/include/ \
- --with-libunwind=$programsPath/libunwind-1.1/ \
- --with-libunwind-include=$programsPath/libunwind-1.1/
+ --with-libunwind-include="$HOME/Programs/libunwind-1.1/include/" \
+ --with-libunwind-lib="$HOME/Programs/libunwind-1.1/ader=/usr/local/include/" \
+ --with-libunwind="$HOME/Programs/libunwind-1.1/" \
+ --with-libunwind-include="$HOME/Programs/libunwind-1.1/"
 
 make -j3
 
 sudo make install
 
-sed -i '1iexport PATH='"$programsPath"'/build/bin:$PATH' ~/.bashrc
+sed -i '1iexport PATH='"$HOME"'/Programs/scorep-3.0/build/bin:$PATH' ~/.bashrc
 
-sed -i '1iexport LD_LIBRARY_PATH='"$programsPath"'/build/lib:$LD_LIBRARY_PATH'  ~/.bashrc
+sed -i '1iexport LD_LIBRARY_PATH='"$HOME"'/Programs/scorep-3.0/build/lib:$LD_LIBRARY_PATH'  ~/.bashrc
 
 source ~/.bashrc
