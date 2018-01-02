6c0a899804337d0cdbcc0985bd1a275f5f8ceae7
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2018-01-02 01:38:47 -0200

    new module to measure PAPI counters from within libnina

---
+++ src/modeling_papi.c
@@ -0,0 +1,133 @@
+#include "modeling_papi.h"
+#ifdef MODELING_PAPI
+#define TF_NAME_LEN 100 // Maximum length of the trace file name
+#define CFG_DEFAULT_PATH "modeling-libnina.cfg"
+extern FILE *fp; //defined somewhere
+
+int *Events;
+long_long *Values;
+int papi_nevents;
+
+void model_error_check (int returnCode){
+  if (returnCode == CONFIG_FALSE){
+    fprintf (stderr, "Failed reading configuration from %s.\n", CFG_DEFAULT_PATH);
+    fflush (stderr);
+    exit(123);
+  }
+}
+
+void model_read_configuration (void)
+{
+  int iteration = 0;
+  FILE *fd = fopen (CFG_DEFAULT_PATH, "r");
+  if (!fd){
+    fprintf (stderr, "it %d Configuration file %s coulnd't be open for reading.\n", iteration, CFG_DEFAULT_PATH);
+    return;
+  }
+  config_t cfg;
+  config_init (&cfg);
+  
+  if (config_read(&cfg, fd) == CONFIG_FALSE){
+    fprintf (stderr,  "Failed to read the configuration file.\n");
+    exit(123);
+  }else{
+    fclose(fd);
+  }
+
+  model_papi_affinity ();
+  
+  model_papi_configuration (&cfg);
+
+  //destroy configuration
+  config_destroy(&cfg);
+}
+
+void model_init (void)
+{
+  model_papi_init ();
+}
+
+void model_papi_init ()
+{
+  PAPI_library_init(PAPI_VER_CURRENT);
+  Events = malloc(PAPI_MAX_HWCTRS*sizeof(int));
+  Values = malloc(PAPI_MAX_HWCTRS*sizeof(long_long));
+  papi_nevents = 0;
+}
+
+void model_papi_start_counters ()
+{
+  PAPI_start_counters(Events, papi_nevents);
+}
+
+void model_papi_stop_counters ()
+{
+  PAPI_stop_counters(Values, papi_nevents);
+}
+
+void model_papi_header ()
+{
+  model_papi_header_with_fp (fp);
+}
+
+void model_papi_header_with_fp (FILE *pfp)
+{
+  int i;
+  //report hw counters
+  for (i = 0; i < papi_nevents; i++){
+    char EventCodeStr[PAPI_MAX_STR_LEN];
+    PAPI_event_code_to_name(Events[i], EventCodeStr);
+    fprintf(pfp, "%s", EventCodeStr);
+    if (i+1 != papi_nevents){
+      fprintf(pfp, " ");
+    }
+  }
+}
+
+void model_papi_report (FILE *zz)
+{
+  model_papi_report_with_fp (zz);
+}
+
+void model_papi_report_with_fp (FILE *pfp)
+{
+  int i;
+  //report hw counters
+  for (i = 0; i < papi_nevents; i++){
+    fprintf(pfp, "%lld", Values[i]);
+    if (i+1 != papi_nevents){
+      fprintf(pfp, " ");
+    }
+  }
+}
+
+void model_papi_affinity ()
+{
+  return;
+  //core affinity
+  cpu_set_t  mask;
+  CPU_ZERO(&mask);
+  CPU_SET(0, &mask);
+  if (sched_setaffinity(0, sizeof(mask), &mask) < 0){
+    fprintf(stderr, "Failed to set core affinity.\n");
+    exit(1);
+  }
+}
+
+void model_papi_configuration (config_t *cfg)
+{
+  config_setting_t *sub_cfg;
+  papi_nevents = 0;
+  sub_cfg = config_lookup (cfg, "papi.counters");
+  const char *papi_event_name;
+  int i;
+  for (i = 0; (papi_event_name = config_setting_get_string_elem (sub_cfg, i)); i++){
+    if (PAPI_event_name_to_code ((char*)papi_event_name, &Events[i]) != PAPI_OK){
+      fprintf (stderr, "Failed to codify papi event [%s].\n", papi_event_name);
+      exit(1);
+    }
+    papi_nevents++;
+  }
+  papi_nevents = i;
+}
+#endif //MODELING_PAPI
---
+++ src/modeling_papi.h
@@ -0,0 +1,42 @@
+#ifndef __MODELING_PAPI_H_
+#define __MODELING_PAPI_H_
+#include <stdio.h>
+#ifdef MODELING_PAPI
+#include <libconfig.h>
+#include <papi.h>
+#define _GNU_SOURCE
+#define __USE_GNU
+#include <sched.h>
+#include <stdlib.h>
+#include <string.h>
+#include <error.h>
+#include <unistd.h>
+
+void model_read_configuration (void);
+void model_init (void);
+void model_papi_init (void);
+void model_papi_start_counters (void);
+void model_papi_stop_counters (void);
+void model_papi_header (void);
+void model_papi_report (FILE *fp);
+void model_papi_report_with_fp (FILE *pfp);
+void model_papi_header (void);
+void model_papi_header_with_fp (FILE *pfp);
+void model_papi_affinity (void);
+void model_papi_configuration (config_t *cfg);
+
+#else
+
+#define model_papi_init(A)
+#define model_papi_start_counters(A)
+#define model_papi_stop_counters(A)
+#define model_papi_header(A)
+#define model_papi_header_with_fp(A)
+#define model_papi_report(A)
+#define model_papi_report_with_fp(A)
+#define model_papi_header(A)
+#define model_papi_affinity(A)
+#define model_papi_configuration(A)
+
+#endif //MODELING_PAPI
+#endif
